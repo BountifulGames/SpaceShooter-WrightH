ad44db035f8b90752f5d6bd9627cb7a1ae37f37f
BountifulGames <hunter.wright98@yahoo.com>
2024-04-02 16:02:19 -0400

    Movement Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 10f;
+    private float rotationSpeed = 100f;
+
     void Start()
     {
         
@@ -13,6 +16,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Bounds();
+    }
+
+    private void Move()
+    {
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalRotation = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up, horizontalRotation * rotationSpeed * Time.deltaTime);
+
+        Vector3 movement = new Vector3(0, 0, verticalInput) * speed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+    }
+
+    private void Bounds()
+    {
+        if (gameObject.transform.position.x < -11)
+        {
+            Vector3 temp = new Vector3(11, transform.position.y, transform.position.z);
+            transform.position = temp;
+        }
+        if (gameObject.transform.position.x > 11)
+        {
+            Vector3 temp = new Vector3(-11, transform.position.y, transform.position.z);
+            transform.position = temp;
+        }
+
+        if (gameObject.transform.position.y < -5.5)
+        {
+            Vector3 temp = new Vector3(transform.position.x, 7.5f, transform.position.z);
+            transform.position = temp;
+        }
+        if (gameObject.transform.position.y > 7.5)
+        {
+            Vector3 temp = new Vector3(transform.position.x, -5.5f, transform.position.z);
+            transform.position = temp;
+        }
     }
 }
